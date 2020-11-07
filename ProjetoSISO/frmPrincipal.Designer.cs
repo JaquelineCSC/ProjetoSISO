ee186df1cfb3b7d83a2171ca58c97b410e7a56c8
Antonio <antonioalves055@hotmail.com>
2020-11-07 11:29:14 -0300

    Alteração Conexao e Dados

--- ProjetoSISO/frmPrincipal.Designer.cs
+++ ProjetoSISO/frmPrincipal.Designer.cs
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.cmdDentista = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.panelForm = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -51,9 +51,43 @@
             this.panel1.Controls.Add(this.button4);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1776, 91);
+            this.panel1.Size = new System.Drawing.Size(1579, 73);
             this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(25, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Projeto SISO";
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Image = global::ProjetoSISO.Properties.Resources.close;
+            this.button4.Location = new System.Drawing.Point(1519, 0);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(60, 73);
+            this.button4.TabIndex = 0;
+            this.button4.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // panel2
             // 
@@ -65,51 +99,31 @@
             this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.cmdDentista);
-            this.panel2.Location = new System.Drawing.Point(-2, 90);
+            this.panel2.Location = new System.Drawing.Point(-2, 72);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(334, 1355);
+            this.panel2.Size = new System.Drawing.Size(297, 1084);
             this.panel2.TabIndex = 1;
             // 
-            // label1
+            // panel3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(28, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(283, 54);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Projeto SISO";
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(2, 1007);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(295, 8);
+            this.panel3.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(76, 1314);
+            this.label2.Location = new System.Drawing.Point(68, 1051);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(117, 27);
+            this.label2.Size = new System.Drawing.Size(99, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "TwoJeitos";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(21, 1285);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(159, 27);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Developed by";
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.White;
-            this.panel3.Location = new System.Drawing.Point(2, 1259);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(332, 10);
-            this.panel3.TabIndex = 3;
             // 
             // button3
             // 
@@ -122,13 +136,25 @@
             this.button3.ForeColor = System.Drawing.Color.White;
             this.button3.Image = global::ProjetoSISO.Properties.Resources.print_60px;
             this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.Location = new System.Drawing.Point(18, 316);
+            this.button3.Location = new System.Drawing.Point(16, 253);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(220, 61);
+            this.button3.Size = new System.Drawing.Size(196, 49);
             this.button3.TabIndex = 0;
             this.button3.Text = "     Relatório";
             this.button3.TextAlign = System.Drawing.ContentAlignment.BottomRight;
             this.button3.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(19, 1028);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Developed by";
             // 
             // button2
             // 
@@ -141,9 +167,10 @@
             this.button2.ForeColor = System.Drawing.Color.White;
             this.button2.Image = global::ProjetoSISO.Properties.Resources.calendar_60px;
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(20, 230);
+            this.button2.Location = new System.Drawing.Point(18, 184);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(281, 61);
+            this.button2.Size = new System.Drawing.Size(250, 49);
             this.button2.TabIndex = 0;
             this.button2.Text = "     Agendamento";
             this.button2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
@@ -160,9 +187,10 @@
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Image = global::ProjetoSISO.Properties.Resources.user_60px;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(26, 135);
+            this.button1.Location = new System.Drawing.Point(23, 108);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(224, 64);
+            this.button1.Size = new System.Drawing.Size(199, 51);
             this.button1.TabIndex = 0;
             this.button1.Text = "     Pacientes";
             this.button1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
@@ -179,54 +207,36 @@
             this.cmdDentista.ForeColor = System.Drawing.Color.White;
             this.cmdDentista.Image = global::ProjetoSISO.Properties.Resources.tooth_64px;
             this.cmdDentista.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.cmdDentista.Location = new System.Drawing.Point(26, 53);
+            this.cmdDentista.Location = new System.Drawing.Point(23, 42);
+            this.cmdDentista.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmdDentista.Name = "cmdDentista";
-            this.cmdDentista.Size = new System.Drawing.Size(224, 66);
+            this.cmdDentista.Size = new System.Drawing.Size(199, 53);
             this.cmdDentista.TabIndex = 0;
             this.cmdDentista.Text = "     Dentistas";
             this.cmdDentista.TextAlign = System.Drawing.ContentAlignment.BottomRight;
             this.cmdDentista.UseVisualStyleBackColor = false;
             this.cmdDentista.Click += new System.EventHandler(this.cmdDentista_Click);
             // 
-            // button4
-            // 
-            this.button4.AutoSize = true;
-            this.button4.BackColor = System.Drawing.Color.Transparent;
-            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button4.Dock = System.Windows.Forms.DockStyle.Right;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Image = global::ProjetoSISO.Properties.Resources.close;
-            this.button4.Location = new System.Drawing.Point(1709, 0);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(67, 91);
-            this.button4.TabIndex = 0;
-            this.button4.TextAlign = System.Drawing.ContentAlignment.BottomRight;
-            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
             // panelForm
             // 
-            this.panelForm.Location = new System.Drawing.Point(338, 97);
+            this.panelForm.Location = new System.Drawing.Point(300, 78);
+            this.panelForm.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelForm.Name = "panelForm";
-            this.panelForm.Size = new System.Drawing.Size(1426, 1336);
+            this.panelForm.Size = new System.Drawing.Size(1268, 1069);
             this.panelForm.TabIndex = 2;
             this.panelForm.Paint += new System.Windows.Forms.PaintEventHandler(this.panelForm_Paint);
             // 
             // frmPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1776, 1445);
+            this.ClientSize = new System.Drawing.Size(1579, 882);
             this.Controls.Add(this.panelForm);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmPrincipal";
             this.Text = "Projeto SISO";
             this.Load += new System.EventHandler(this.Form1_Load);
